0363a88a43f98ee85415efe5bd2136e477350cf6
Valdis <v.beikmanis@gmail.com>
2024-12-05 10:42:09 +0000

    initial commit

---
+++ src/main/java/com/northcoders/recordapi/RecordApiApplication.java
@@ -0,0 +1,13 @@
+package com.northcoders.recordapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecordApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecordApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/northcoders/recordapi/RecordApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.northcoders.recordapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecordApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
